e010cb6776370865d58eb5b421055302a82c996c
brenda-soto <bsoto@pursuit.org>
2024-07-20 11:36:46 -0400

    More console.logs

--- controllers/countriesController.js
+++ controllers/countriesController.js
@@ -6,7 +6,7 @@ const { getAllCountries } = require("../queries/countries");
 //INDEX http://localhost:3003/api/countries
 countries.get("/", async (req, res) => {
   const allCountries = await getAllCountries();
-
+  console.log("All countries in GET", allCountries);
   if (allCountries[0]) {
     res.status(200).json(allCountries);
   } else {
