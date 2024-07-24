b4b5762c24951d40993f5eea892eba932050efb1
brenda-soto <bsoto@pursuit.org>
2024-07-24 12:34:53 -0400

    Cron

--- app.js
+++ app.js
@@ -28,13 +28,16 @@ const getWorldNews = async () => {
   }
 };
 
-// const job = new CronJob(
-//   "0 7 * * * *", // cronTime
-//   getWorldNews, // onTick
-//   null, // onComplete
-//   true, // start
-//   "America/New_York" // timeZone
-// );
+const job = new CronJob(
+  "38 12 * * * *", // cronTime
+  getWorldNews, // onTick
+  null, // onComplete
+  true, // start
+  "America/New_York", // timeZone
+  function () {
+    console.log(new Date());
+  }
+);
 
 // job.start();
 
